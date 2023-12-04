af352d382c479def3d24b6be23df3e5d8fa7cf35
Matheus12Santos <matheus.vieira137@etec.sp.gov.br>
2023-12-04 11:16:29 -0300

    Estilizando as bordas

--- src/js/script.js
+++ src/js/script.js
@@ -8,6 +8,7 @@ function showPokemonList(){
         if (Response.status == 200){
             listaPokemons = Response.json().then( json => {json.results.map( pokemon => { 
                 let liPokemon = document.createElement("li")
+                liPokemon.className = "pokemon";
                 let liPokemonImg = document.createElement("img")
                 fetch(pokemon.url).then(Response => { pokemonImg = Response.json().then( (pokemon) => {(liPokemonImg.src = pokemon.sprites.front_default)})})
                 liPokemon.innerHTML = pokemon.name
