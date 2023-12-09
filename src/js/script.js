3556df88b7c82dc840f89af73eb2d8849647793f
Matheus12Santos <matheus.vieira137@etec.sp.gov.br>
2023-12-08 22:23:15 -0300

    Busca por id, modal

--- src/js/script.js
+++ src/js/script.js
@@ -2,7 +2,7 @@ const api = 'https://pokeapi.co/api/v2/pokemon/'
 const apiImg = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'
 
 
-function showPokemonList(){
+function showPokemonList() {
     let listaPokemons = []
     let count = 1
     fetch(api + '?limit=251').then( Response => {
@@ -25,18 +25,42 @@ function showPokemonList(){
 
 showPokemonList()
 
-function getPokemonById(id){
-    fetch(api + id).then( response => {
-        if (response.status == 200){
-            response.json().then( pokemon => {
-                let liPokemon = document.createElement("li")
-                liPokemon.className = "pokemon";
-                let liPokemonImg = document.createElement("img")
-                liPokemonImg.src = pokemon.sprites.front_default;
-                liPokemon.innerHTML = pokemon.name
-                liPokemon.appendChild(liPokemonImg)
-                document.getElementById("ListaPokemons").appendChild(liPokemon)
-            })
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('searchBar').addEventListener('keydown', function(e) {
+        if (e.keyCode === 13) {
+            searchIdPokemon();
         }
-    })
+    });
+});
+
+function searchIdPokemon() {
+    var id = document.getElementById('searchBar').value;
+    if (id > 0 && id < 251) {
+        fetch(api + id)
+            .then(response => response.json())
+            .then(pokemon => {
+                var modalContent = '<br><img src="' + pokemon.sprites.front_default + '">' +
+                    'Nome: ' + pokemon.name +
+                    '<br>ID: ' + pokemon.id +
+                    '<br>Altura: ' + pokemon.height +
+                    '<br>Peso: ' + pokemon.weight +
+                    '<br>Habilidades: ' + pokemon.abilities.map(a => a.ability.name).join(', ') +
+                    '<br>Tipos: ' + pokemon.types.map(t => t.type.name).join(', ');
+                document.getElementById('selection').innerHTML = modalContent;
+                console.log(pokemon.name);
+            });
+        var modal = document.getElementById('tela');
+        modal.style.display = "block";
+    }
+    else {
+        
+    }
 }
+
+
+window.addEventListener('click', function(event) {
+    var modal = document.getElementById('myModal');
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+});
