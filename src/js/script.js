6f2e8fdd55a8a45d1a378a4c1e9a35dfb55bd35d
Matheus12Santos <matheus.vieira137@etec.sp.gov.br>
2023-12-09 23:55:05 -0300

    adicionando mochila/captura

--- src/js/script.js
+++ src/js/script.js
@@ -1,26 +1,79 @@
 const api = 'https://pokeapi.co/api/v2/pokemon/'
 const apiImg = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'
 
+let backpack = [];
 
 function showPokemonList() {
-    let listaPokemons = []
-    let count = 1
-    fetch(api + '?limit=251').then( Response => {
-        if (Response.status == 200){
-            listaPokemons = Response.json().then( json => {json.results.map( pokemon => { 
-                let liPokemon = document.createElement("li")
-                liPokemon.className = "pokemon";
-                let liPokemonImg = document.createElement("img")
-                fetch(pokemon.url).then(Response => { pokemonImg = Response.json().then( (pokemon) => {(liPokemonImg.src = pokemon.sprites.front_default)})})
-                liPokemon.innerHTML = count + '. ' + pokemon.name
-                liPokemon.appendChild(liPokemonImg)
-                document.getElementById("ListaPokemons").appendChild(liPokemon)
-                count++;
-            })
-            return json})
-            return listaPokemons
+    let listaPokemons = [];
+    let count = 1;
+    fetch(api + '?limit=251').then(Response => {
+        if (Response.status == 200) {
+            listaPokemons = Response.json().then(json => {
+                json.results.map(pokemon => {
+                    let liPokemon = document.createElement("li");
+                    liPokemon.className = "pokemon";
+                    let liPokemonImg = document.createElement("img");
+                    liPokemonImg.className = "pokemon-img";
+                    let button = document.createElement('button');
+                    button.textContent = 'Capturar';
+                    button.className = 'capture-button';
+                    button.addEventListener('click', function() {
+                        if (backpack.length < 6) {
+                            if (!backpack.find(p => p.name === pokemon.name)) { 
+                                fetch(pokemon.url).then(Response => {
+                                    pokemonImg = Response.json().then((pokemon) => {
+                                        liPokemonImg.src = pokemon.sprites.front_default;
+                                        pokemon.image = liPokemonImg.src;
+                                        backpack.push(pokemon);
+                                        updateBackpackModal();
+                                    });
+                                });
+                            } else {
+                                alert('Você já capturou este Pokémon!');
+                            }
+                        } else {
+                            alert('Sua mochila está cheia!');
+                        }
+                    });
+                    fetch(pokemon.url).then(Response => {
+                        pokemonImg = Response.json().then((pokemon) => {
+                            liPokemonImg.src = pokemon.sprites.front_default;
+                        });
+                    });
+                    liPokemon.innerHTML = count + '. ' + pokemon.name;
+                    liPokemon.appendChild(liPokemonImg);
+                    liPokemon.appendChild(button);
+                    document.getElementById("ListaPokemons").appendChild(liPokemon);
+                    count++;
+                });
+                return json;
+            });
+            return listaPokemons;
         }
-    })
+    });
+}
+
+function updateBackpackModal() {
+    const backpackModal = document.getElementById('backpack_modal');
+    backpackModal.innerHTML = '';
+    const counter = document.createElement('p');
+    counter.textContent = `Pokémon na mochila: ${backpack.length}/6`;
+    backpackModal.appendChild(counter);
+    const grid = document.createElement('div');
+    grid.className = 'pokemon-grid'; 
+    backpack.forEach(pokemon => {
+        const div = document.createElement('div');
+        const p = document.createElement('p');
+        p.textContent = pokemon.name;
+        p.className = "pokemon-name";
+        const img = document.createElement('img');
+        img.src = pokemon.image;
+        img.className = "pokemon-img";
+        div.appendChild(p);
+        div.appendChild(img);
+        grid.appendChild(div);
+    });
+    backpackModal.appendChild(grid);
 }
 
 showPokemonList()
@@ -53,10 +106,13 @@ function searchIdPokemon() {
         modal.style.display = "block";
     }
     else {
-        
+        /* var modalContent = '<br><img id="not_found" src="./src/imagens/pokemon_not_found.png" alt="pokemon_nao_encontrado">' +
+            'Pokemon não encontrado';
+        document.getElementById('selection').innerHTML = modalContent;
+        var modal = document.getElementById('tela');
+        modal.style.display = "block"; */
     }
 }
-
 
 window.addEventListener('click', function(event) {
     var modal = document.getElementById('myModal');
@@ -64,3 +120,10 @@ window.addEventListener('click', function(event) {
         modal.style.display = "none";
     }
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("backpack").addEventListener("click", function() {
+        document.getElementById("backpack_modal").style.display = "block";
+    });
+});
+
